ed06947cf353efdeb302bf07d94050d60f1c91a9
Lucas De Lia <lucasdelia018@gmail.com>
2024-08-08 11:02:06 -0300

    primer commit

---
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Navbar} from '../../ui';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import {MarvelPage , DCPage, Search,Hero} from '../pages';
+export const HeroesRoutes = () => {
+  return (
+    <>
+        <Navbar/>
+        <div className='container'>
+        <Routes>
+            <Route path='marvel' element = {<MarvelPage/>}/>
+            <Route path='dc' element = {<DCPage/>}/>
+            <Route path='/' element = {<Navigate to = '/marvel'/>}/>
+
+            <Route path='search' element = {<Search/>}/>
+            <Route path='hero' element = {<Hero/>}/>
+        </Routes>
+        </div>
+        
+    </>
+  )
+}
